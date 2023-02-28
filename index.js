ce89d0214e0d0e626d1e58d5c1016d261b7a85b2
Arpan Waddewar <arpanwaddewar3211@gmail.com>
2023-02-28 13:35:15 +0530

    ccccc

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.use(cors());
 // Calling the database connection function
 connection(); 
 
-app.use(express.static(path.join(__dirname,'.//frontend/build')));
+app.use(express.static(path.join(__dirname+'./frontend/build')));
 app.get("*",function(req, res){
-    res.sendFile(path.join(__dirname , ".//frontend/build/index.html"))
+    res.sendFile(path.join(__dirname + "./frontend/build/index.html"))
 })
 
 app.use("/api", require("./routes")); 
